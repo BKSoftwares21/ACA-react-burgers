a4b20b5b8b9f20ca1d35b14ee8966411c36aa34f
BKSoftwares21 <benitakazadi56@gmail.com>
2024-07-19 19:00:28 +0200

    styles too add to style sheet and new buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+
+import './components/BurgerStyles.css' 
 function App() {
 
 
   return (
-    <>
-      <div>
+    <><h1>ACA Burger</h1>
+        <h3>By benita Kazadi</h3>
+      <div className= 'burgerdisplay'>
         
+        
+
+<button>Add Tomato</button>
+<button>Add Lettuce</button>
+<button>Add Meat</button>
+<button>Add Cheese</button>
+
       </div>
       
     </>
